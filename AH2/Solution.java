c50b025a8fbd425a6ccd2ec310cc6ef145174801
bw26 <147175761+bw26@users.noreply.github.com>
2023-10-10 15:23:45 -0400

    Add files via upload

--- AH2/Solution.java
+++ AH2/Solution.java
@@ -11,8 +11,7 @@ public class Solution {
     }
     public static boolean isPrime(int number){
         if (number < 2) {return false;}
-        if (number == 2) {return true;}
-        for(int i = 3; i <= Math.sqrt(number); i++){
+        for(int i = 2; i <= Math.sqrt(number); i++){
             if (number % i == 0){return false;}
         }
         return true;
